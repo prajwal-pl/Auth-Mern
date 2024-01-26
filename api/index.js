23a546920c386b8cb90746a61c5b0fbb15592cd5
Prajwal-PL <prajwalpl096@gmail.com>
2024-01-26 15:41:57 +0530

    created database connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected');
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000, ()=>{
     console.log('Server running on the port: 3000');
