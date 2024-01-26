9285ef6a235b82115cde4f2a7cd57f6e6d127658
Prajwal-PL <prajwalpl096@gmail.com>
2024-01-26 15:19:53 +0530

    created a header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import About from './pages/About'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
